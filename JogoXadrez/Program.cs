633fcfce35a7adfeada0c62d217195a6dd4f38f3
ZghostPlayer <zmanciofilho@gmail.com>
2023-02-28 09:52:57 -0300

    Criando a classe Posicao

--- JogoXadrez/Program.cs
+++ JogoXadrez/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using JogoXadrez.tabuleiro;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        Posicao P;
+
+        P = new Posicao(3, 4);
+
+        Console.WriteLine("Posicao " + P);
+
+        Console.ReadLine();
+    }
+}
