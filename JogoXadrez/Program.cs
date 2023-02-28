2d6f03e108617f477c671969151f0c41c9c3a054
ZghostPlayer <zmanciofilho@gmail.com>
2023-02-28 11:17:20 -0300

    Criando a classe PosicaoXadrez

--- JogoXadrez/Program.cs
+++ JogoXadrez/Program.cs
@@ -2,27 +2,17 @@
 using JogoXadrez.tabuleiro;
 using JogoXadrez.xadrez;
 
-internal class Program
+ class Program
 {
     private static void Main(string[] args)
     {
-        try
-        {
-            Tabuleiro tab = new Tabuleiro(8, 8);
+        PosicaoXadrez pos = new PosicaoXadrez('c', 7);
 
-            tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 9));
-            tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-            tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
+        Console.WriteLine(pos);
 
-            Tela.ImprimirTabuleiro(tab);
+        Console.WriteLine(pos.toPosicao());
 
-            Console.ReadLine();
-        }
-        catch(TabuleiroException e)
-        {
-            Console.WriteLine(e.Message);
-        }
-
-        Console.ReadLine() ;
+        Console.ReadLine();
+        
     }
 }
